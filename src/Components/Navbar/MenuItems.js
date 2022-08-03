f35a2435ddba29207ec9fce3ecd4dd7e17817b79
Hemraj-k <83349808+Hemraj-k@users.noreply.github.com>
2022-08-03 16:13:40 +0530

    new

---
+++ src/Components/Navbar/MenuItems.js
@@ -0,0 +1,29 @@
+const menuItems = [
+  {
+    title: 'Home',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'About',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Services',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Gallery',
+    url: '#',
+    cName: 'nav-links',
+  },
+  {
+    title: 'Contact',
+    url: '#',
+    cName: 'nav-links',
+  },
+];
+
+export default menuItems;
