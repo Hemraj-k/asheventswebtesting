88d477bc9c43b0c4b60d0538db771e1835e3e2ed
Hemraj-k <83349808+Hemraj-k@users.noreply.github.com>
2022-08-11 11:37:17 +0530

    new commit

--- src/Components/Navbar/Contact.js
+++ src/Components/Navbar/Contact.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../Styles/Contact.css';
 function Contact() {
+  function TextInput({ type, label, pattern }) {
+    const [value, setValue] = useState('');
+
+    function handleChange(e) {
+      setValue(e.target.value);
+    }
+
+    return (
+      <div className="input-container">
+        <input type={type} value={value} onChange={handleChange} />
+        <label className={value && 'filled'}>{label}</label>
+      </div>
+    );
+  }
   return (
     <div className="Contact">
-      <h1>Contact</h1>
+      <div>Contact us</div>
+      <div className="ContactContainer">
+        <div className="ContactForm">
+          <form>
+            <div className="contact-form">
+              <TextInput label="Full Name *" type="text" />
+              <TextInput label="Phone Number *" type="tel" required />
+              <TextInput label="Email ID " type="email" />
+              <textarea
+                name="Message *"
+                className="contact-message"
+                placeholder="Enter your message here"
+                required
+              ></textarea>
+              <button type="submit" className="send-button">
+                Send
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="ContactDetails"></div>
+      </div>
     </div>
   );
 }
